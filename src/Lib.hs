4de37c313bfa440e9eb1059f8fa5a5546eec09a9
Levi Soto <levisoto.337@gmail.com>
2017-08-05 06:21:06 -0500

    I improve the problem 2. Now the function is fast

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,12 +7,20 @@ someFunc = putStrLn "someFunc"
 m3and5 :: Integer -> Integer
 m3and5 num = sum [x | x <- [1..num-1], mod x 3 == 0 || mod x 5 == 0]
 
---Problem 2 "sum of even-values from fibonacci's serie "
-fibonacci :: Integer -> Integer
-fibonacci num = sum [x | x <- (map fibo [1..num]), mod x 2 == 0]
-  where
-    fibo 1 = 1
-    fibo 2 = 2
-    fibo n = fibo (n - 2) + fibo (n - 1)
---The last term of fibonacci is the 32th element == 3524578
+--Problem 2 "sum of even-values from foundfibo's serie "
+foundfibo :: Integer
+foundfibo = sum $ filter (even) $ filter (<4000000) $ fibo2 100
+  --where
+    --fibo 1 = 1
+    --fibo 2 = 2
+    --fibo n = fibo (n - 2) + fibo (n - 1)
+--The last term of foundfibo is the 32th element == 3524578
 
+fibo2 :: Integer -> [Integer]
+fibo2 0 = [0]
+fibo2 1 = [1]
+fibo2 2 = [1,1]
+fibo2 n
+  | n > 2 = addnext $ fibo2 (n-1)
+    where
+      addnext list = list++[(list !! ((length list)-2))+(list !! ((length list)-1))]
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,7 @@ skipsSuite :: TestTree
 skipsSuite = testGroup "0three rec poly TEST"
               [ testGroup "Test of --skips-- function" $
                   [testCase "problem 1" $ m3and5 1000 @?= 233168
-                  ,testCase "problem 2" $ fibonacci 8 @?= 44
+                  ,testCase "problem 2" $ foundfibo 8 @?= 44
                   ]
               ]
 
