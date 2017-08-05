6629d21105d5e9ff8af63bae61c9ba6b52cd22fb
Levi Soto <levisoto.337@gmail.com>
2017-08-04 21:10:13 -0500

    I finished the problem 1 and 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,3 +2,17 @@ module Lib where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+--Problem 1 "Multiples of 3 and 5"
+m3and5 :: Integer -> Integer
+m3and5 num = sum [x | x <- [1..num-1], mod x 3 == 0 || mod x 5 == 0]
+
+--Problem 2 "sum of even-values from fibonacci's serie "
+fibonacci :: Integer -> Integer
+fibonacci num = sum [x | x <- (map fibo [1..num]), mod x 2 == 0]
+  where
+    fibo 1 = 1
+    fibo 2 = 2
+    fibo n = fibo (n - 2) + fibo (n - 1)
+--The last term of fibonacci is the 32th element == 3524578
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,16 +8,8 @@ import Lib
 skipsSuite :: TestTree
 skipsSuite = testGroup "0three rec poly TEST"
               [ testGroup "Test of --skips-- function" $
-                  [testCase "test 1" $ skips "ABCD" @?=["ABCD","BD","C","D"]
-                  ,testCase "test 2" $ skips "hello!" @?=["hello!","el!","l!","l","o","!"]
-                  ,testCase "test 3" $ skips [1] @?=[[1]]
-                  ,testCase "test 4" $ skips [True,False] @?= [[True,False], [False]]
-                  ]
-
-            , testGroup "Test of --localMaxima-- function"
-                  [testCase "test 1" $ localMaxima [2,9,5,6,1] @?=[9,6]
-                  ,testCase "test 2" $ localMaxima [2,3,4,1,5] @?=[4]
-                  ,testCase "test 3" $ localMaxima [1,2,3,4,5] @?=[]
+                  [testCase "problem 1" $ m3and5 1000 @?= 233168
+                  ,testCase "problem 2" $ fibonacci 8 @?= 44
                   ]
               ]
 
